90cf363f6c3f1b0c28abe432294f4a590aca1411
Nathan Douglas <ndouglas@devontechnologies.com>
2015-11-03 19:10:54 -0600

    Refactors simple test around a dedicated class.

--- SwiftNetServiceTests/BrowserDelegateTests.swift
+++ SwiftNetServiceTests/BrowserDelegateTests.swift
@@ -31,6 +31,26 @@ class ServiceDelegate : NSObject, NSNetServiceDelegate {
 
 }
 
+class TestService {
+    var UUID : String
+    var type : String
+    var service : NSNetService
+    var delegate : ServiceDelegate?
+    
+    init(port : Int32) {
+        self.UUID = NSUUID().UUIDString
+        self.type = "_\(self.UUID)._tcp"
+        self.service = NSNetService(domain: "local", type: self.type, name: self.UUID, port: port)
+    }
+    
+    func publishAndFulfillExpectation(expectation: XCTestExpectation) {
+        self.delegate = ServiceDelegate(expectation: expectation)
+        self.service.delegate = self.delegate
+        self.service.publish()
+    }
+    
+}
+
 
 class BrowserDelegateTests: XCTestCase {
     
@@ -44,13 +64,9 @@ class BrowserDelegateTests: XCTestCase {
     
     func testBrowserDelegate() {
         // We're going to publish a net service and see if we find it.
-        let aUUID = NSUUID().UUIDString
-        let aType = "_\(aUUID)._tcp"
-        let aService = NSNetService(domain: "local", type: aType, name: aUUID, port: 2015)
+        let myTestService = TestService(port: 2015)
         let expectation = self.expectationWithDescription("published")
-        let aDelegate = ServiceDelegate(expectation: expectation)
-        aService.delegate = aDelegate
-        aService.publish()
+        myTestService.publishAndFulfillExpectation(expectation)
         self.waitForExpectationsWithTimeout(5.0, handler: nil)
         // Getting this far means that the service was created and published successfully.
     }
