e37ba94a8a210c7426a5799d0ecc6af80695e1b4
Nathan Douglas <ndouglas@devontechnologies.com>
2015-11-03 19:01:26 -0600

    Starting to work on unit tests.

--- SwiftNetServiceTests/BrowserDelegateTests.swift
+++ SwiftNetServiceTests/BrowserDelegateTests.swift
@@ -9,6 +9,29 @@
 import XCTest
 @testable import SwiftNetService
 
+class ServiceDelegate : NSObject, NSNetServiceDelegate {
+    var expectation : XCTestExpectation
+    
+    init(expectation : XCTestExpectation) {
+        self.expectation = expectation
+    }
+
+    @objc func netService(sender: NSNetService, didNotPublish errorDict: [String : NSNumber]) {
+        XCTFail("Failed to publish service: \(errorForErrorDictionary(errorDict))")
+    }
+
+    @objc func netServiceDidPublish(sender: NSNetService) {
+        self.expectation.fulfill()
+        NSLog("Did publish service: \(sender)")
+    }
+
+    @objc func netServiceWillPublish(sender: NSNetService) {
+        NSLog("Will publish service: \(sender)")
+    }
+
+}
+
+
 class BrowserDelegateTests: XCTestCase {
     
     override func setUp() {
@@ -20,7 +43,17 @@ class BrowserDelegateTests: XCTestCase {
     }
     
     func testBrowserDelegate() {
-        // We're going to perform a simple test here -- we're going to 
+        // We're going to publish a net service and see if we find it.
+        let aUUID = NSUUID().UUIDString
+        let aType = "_\(aUUID)._tcp"
+        let aService = NSNetService(domain: "local", type: aType, name: aUUID, port: 2015)
+        let expectation = self.expectationWithDescription("published")
+        let aDelegate = ServiceDelegate(expectation: expectation)
+        aService.delegate = aDelegate
+        aService.publish()
+        self.waitForExpectationsWithTimeout(5.0, handler: nil)
+        // Getting this far means that the service was created and published successfully.
     }
+   
     
 }
